0219d1aefe9d158b7bbb320b53ce69171412d48d
Naveen Kumar Konam <52211514+naveenkonam1985@users.noreply.github.com>
2023-09-11 19:23:41 +0530

    Update streamlit_app.py
    added page config

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,8 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+st.set_page_config(page_title="Table Crawler", page_icon=None, layout="wide", initial_sidebar_state="auto", menu_items=None)
+
 connection = False
 
 # Sidebar config
